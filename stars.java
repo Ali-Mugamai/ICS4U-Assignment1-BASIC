e0e41f7108ee2fc93ef49f118b7653f84b2b82cc
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2024-10-07 03:57:58 +0000

    minor changes to java

--- stars.java
+++ stars.java
@@ -1,66 +1,53 @@
-/*
-* this program runs a number guessing game
-*
-* @author  Ali Mugamai
-* @version 1.0
-* @since   2021-09-24
-*/
-
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class NumberGuessingGame {
+
+    private static final int LIMIT = 100;
+    private static final int MAX_GUESSES = 7;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int limit = 100;
-        int numOfGuesses = 7;
-
-        while (true) {
-            System.out.println("STARS - A NUMBER GUESSING GAME");
+        boolean guessedCorrectly;
+        do {
+            // Ask for instructions
             System.out.println("Do you want instructions (YES or NO)?");
-            String UserInputInstructions = scanner.nextLine().toUpperCase();
+            String instructionResponse = scanner.nextLine();
 
-            if ( UserInputInstructions.equals("YES")) {
-                printInstructions(limit, maxGuesses);
+            if (instructionResponse.equalsIgnoreCase("YES")) {
+                printInstructions();
             }
-
-            int targetNumber = random.nextInt(limit) + 1;
-            System.out.println("OK, I am thinking of a number from 1 to " + limit + ". Start guessing.");
-
-            boolean guessedCorrectly = false;
-
-            for (int guessCount = 1; guessCount <= maxGuesses; guessCount++) {
+            int targetNumber = random.nextInt(LIMIT) + 1;
+            System.out.println("OK, I am thinking of a number from 1 to " + LIMIT + ". Start guessing.");
+            guessedCorrectly = false;
+            for (int guessCount = 1; guessCount <= MAX_GUESSES; guessCount++) {
                 System.out.print("Your guess: ");
                 int guess = scanner.nextInt();
                 scanner.nextLine();
-
                 if (guess == targetNumber) {
                     guessedCorrectly = true;
                     break;
                 }
-
                 int difference = Math.abs(targetNumber - guess);
-                printIn(difference);
+                printStars(difference);
             }
-
+            // check if win or lose
             if (!guessedCorrectly) {
-                System.out.println("SORRY, THAT WAS " + maxGuesses + " GUESSES. NUMBER WAS " + targetNumber);
+                System.out.println("SORRY, THAT WAS " + MAX_GUESSES + " GUESSES. NUMBER WAS " + targetNumber);
             } else {
-                System.out.println("!!!");
-                System.out.println("YOU GOT IT IN " + maxGuesses + " GUESSES! LET'S PLAY AGAIN...");
+                System.out.println("YOU GOT IT IN " + MAX_GUESSES + " GUESSES! LET'S PLAY AGAIN ...");
             }
-
             System.out.println();
-        }
+        } while (true);
     }
 
-    private static void printInstructions(int limit, int maxGuesses) {
-        System.out.println("I am thinking of a whole number from 1 to " + limit);
+    private static void printInstructions() {
+        System.out.println("I am thinking of a whole number from 1 to " + LIMIT);
         System.out.println("Try to guess my number. After you guess, I will type one or more stars (*).");
         System.out.println("The more stars I type, the closer you are to my number.");
         System.out.println("One star (*), means far away. Seven stars (******* ) means really close!");
-        System.out.println("You get " + maxGuesses + " guesses.");
+        System.out.println("You get " + MAX_GUESSES + " guesses.");
     }
 
     private static void printStars(int difference) {
@@ -73,7 +60,7 @@ public class NumberGuessingGame {
         else if (difference > 2) stars = 6;
         else if (difference == 1) stars = 7;
 
-        for (int guess = 0; guess < stars; i++) {
+        for (int guess = 0; guess < stars; guess++) {
             System.out.print("*");
         }
         System.out.println();
